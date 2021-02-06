999f095297eef17f41a80c04c0fd9170e43a5c7a
anshal ashish <91808@LAPTOP-ICEIOISQ. (none)>
2021-02-06 12:56:44 +0530

    added superadmin, routes,validators aand completed signup & signin process

---
+++ src/index.server.js
@@ -0,0 +1,36 @@
+//importing all the modules.
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+require("dotenv").config();
+
+//import routes
+const authRoutes = require('../routes/routes');
+
+//start app
+const app = express();
+
+//connect database
+mongoose
+ .connect(process.env.DATABASE,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(() => console.log('Database Connected'));
+
+//middlewares
+app.use (bodyparser.json());
+app.use (cors());
+
+//routes middleware
+app.use('/api', authRoutes);
+
+
+//define port
+const port = process.env.PORT||6060;
+
+//listen to the server
+app.listen(port, () =>{
+    console.log(`server is running on port ${port}`)
+})
